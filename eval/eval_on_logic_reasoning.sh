3571d8aa5d82b022767dddd81bf83699b79a4a8c
y726wang <ubo.wang.sunny@gmail.com>
2025-06-04 15:02:08 -0400

    initialize

--- eval/eval_on_logic_reasoning.sh
+++ eval/eval_on_logic_reasoning.sh
@@ -4,6 +4,8 @@ model_path="../../models/Qwen2.5-Math-7B" # the path to your checkpoints
 output_path="../eval_logic_results/"
 summary_path="../eval_logic_results/summary.txt"
 
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
 cd Logic_Reasoning_Evaluation/
 bash run_evaluation.sh $model_path $output_path $summary_path
 
--- eval/eval_on_math_reasoning.sh
+++ eval/eval_on_math_reasoning.sh
@@ -4,6 +4,8 @@ model_path="../../models/Qwen2.5-Math-7B" # the path to your checkpoints
 output_path="../eval_math_results/"
 summary_path="../eval_math_results/summary.txt"
 
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
 cd Math_Reasoning_Evaluation/scripts
 bash evaluate_qwen.sh $model_path $output_path $summary_path
 
