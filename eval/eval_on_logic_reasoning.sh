665293e0b32bc5492a5305150b9ef36899567b62
y726wang <ubo.wang.sunny@gmail.com>
2025-06-04 15:13:51 -0400

    initialize

--- eval/eval_on_logic_reasoning.sh
+++ eval/eval_on_logic_reasoning.sh
@@ -1,11 +1,13 @@
 set -x
 
-model_path="../../models/Qwen2.5-Math-7B" # the path to your checkpoints
-output_path="../eval_logic_results/"
-summary_path="../eval_logic_results/summary.txt"
+model_path="../../../models/Qwen2.5-Math-7B" # the path to your checkpoints
+output_path="../../eval_logic_results/"
+summary_path="../../eval_logic_results/summary.txt"
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 cd Logic_Reasoning_Evaluation/
+
+mkdir -p output_path
 bash run_evaluation.sh $model_path $output_path $summary_path
 
--- eval/eval_on_math_reasoning.sh
+++ eval/eval_on_math_reasoning.sh
@@ -1,12 +1,14 @@
 set -x
 
-model_path="../../models/Qwen2.5-Math-7B" # the path to your checkpoints
-output_path="../eval_math_results/"
-summary_path="../eval_math_results/summary.txt"
+model_path="../../../models/Qwen2.5-Math-7B" # the path to your checkpoints
+output_path="../../eval_math_results/"
+summary_path="../../eval_math_results/summary.txt"
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 cd Math_Reasoning_Evaluation/scripts
+
+mkdir -p output_path
 bash evaluate_qwen.sh $model_path $output_path $summary_path
 
 
